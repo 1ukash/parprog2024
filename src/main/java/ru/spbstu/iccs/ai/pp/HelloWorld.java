53b81ef59b7220a2afd516fb4f751a9f219a8383
Alexey Lukashin <alexey.lukashin@crplab.ru>
2024-09-18 11:42:02 +0300

    first lecture and very simple example

---
+++ src/main/java/ru/spbstu/iccs/ai/pp/HelloWorld.java
@@ -0,0 +1,22 @@
+package ru.spbstu.iccs.ai.pp;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hey there!");
+        
+        
+        int a = 0;
+        a ++;
+        
+        String s = new String("s");
+        String s2 = "s";
+       // s2 = s + s2;
+        
+        if (s.equals(s2)) {
+        	System.out.println("Wow ");
+        }
+        
+    }
+
+}
---
+++ src/main/java/ru/spbstu/iccs/ai/pp/lecture1/HelloWorldNew.java
@@ -0,0 +1,12 @@
+package ru.spbstu.iccs.ai.pp.lecture1;
+
+import ru.spbstu.iccs.ai.pp.HelloWorld;
+
+
+public class HelloWorldNew {
+
+    public static void main(String[] args) {
+       HelloWorld.main(args);
+    }
+
+}
